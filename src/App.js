19703a2e71ae490c4ae7b0604cdcff95989d5d8d
Steven Winter <stevenwinter.me@gmail.com>
2020-02-22 23:46:51 -0600

    Added Shop route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
-
-import HomePage from './pages/homepage/homepage.component';
-
+ 
+import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+ 
 function App() {
-  return <div>
-    <HomePage />
-  </div>
+  return (
+    <div>
+    <Switch>
+      <Route exact path='/' component= {Homepage}   />
+      <Route path='/shop' component= {ShopPage}   />
+    </Switch>
+        
+    </div>
+  );
 }
-
+ 
 export default App;
